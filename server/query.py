2a8c69e197133d43ce4c12883b51c68e0b13b129
Sammy Thomas <sgpthomas@gmail.com>
2023-03-27 09:51:03 -0500

    some graphcs

--- server/query.py
+++ server/query.py
@@ -2,24 +2,45 @@ from pathlib import Path
 import json
 import pandas as pd
 from dfply import filter_by, X, dfpipe, select, mutate
+from functools import reduce
 
 
-# def cmp_params(a):
-#     return list(map(int, a.replace("_", "x").split("x")))
-# res = sorted(res, key=lambda a: (cmp_params(a[1]), a[2]))
-# for r in res:
-#     print(r)
-# out = Path("..") / "experiments" / "cycle-estimation" / "2d-conv-3x3_3x3_iter.csv"
-# with out.open("w+") as f:
-#     f.write("index,costfn,cost,nodes\n")
-#     for row in res:
-#         f.write(",".join(row))
-#         f.write("\n")
+def cmp_params(a):
+    if a.name == "params":
+        return a.map(lambda x: x.replace("_", "x").split("x")).map(
+            lambda l: reduce(lambda x, y: x * y, map(int, l))
+        )
+    else:
+        return a
+
 
 @dfpipe
 def pivot_table(df, **kwargs):
     df = pd.pivot_table(df, **kwargs)
     return df.reset_index(drop=True, names=["index"])
+
+
+@dfpipe
+def reset_index(df, **kwargs):
+    return df.reset_index(**kwargs)
+
+
+@dfpipe
+def sort_values(df, **kwargs):
+    return df.sort_values(**kwargs)
+
+
+@dfpipe
+def to_csv(df, path, **kwargs):
+    df.to_csv(path, **kwargs)
+    print(f"Wrote {path}")
+    return df
+
+
+@dfpipe
+def display(df):
+    print(df.to_string())
+    return df
 
 
 def exp_iter(name, date=None):
@@ -31,11 +52,13 @@ def exp_iter(name, date=None):
         if "metadata" not in config:
             continue
 
-        if all([
+        if all(
+            [
                 "Mar20" in config["date"],
                 "expanding" in config["metadata"]["rules.json"],
-                name in config["name"]
-        ]):
+                name in config["name"],
+            ]
+        ):
             df = (
                 pd.read_csv(exp_path / "data.csv")
                 >> filter_by((X.name == "nodes") | (X.name == "cost"))
@@ -44,7 +67,7 @@ def exp_iter(name, date=None):
                     index=["phase", "iteration"],
                     columns=["name"],
                     values="value",
-                    sort=False
+                    sort=False,
                 )
                 >> mutate(pruning=config["metadata"]["alt_cost"])
                 >> select(["pruning", "cost", "nodes"])
@@ -58,6 +81,55 @@ def exp_iter(name, date=None):
     print(f"Wrote {out}")
 
 
+def compile_est_cycles():
+    res = []
+    for config_path in Path("completed").glob("**/config.json"):
+        exp_path = Path(config_path.parents[0])
+        config = json.load(config_path.open("r"))
+
+        if "metadata" not in config:
+            continue
+
+        name, params = config["name"].split("_", 1)
+        cycles_csv = exp_path / "results" / f"{name}.csv"
+
+        if all([
+                "Mar20" in config["date"],
+                cycles_csv.exists(),
+        ]):
+            ruleset = Path(config["metadata"]["rules.json"]).stem
+            print(ruleset)
+            df = (
+                pd.read_csv(cycles_csv)
+                >> mutate(
+                    benchmark=name,
+                    params=params,
+                    ruleset=ruleset,
+                    greedy=config["metadata"]["alt_cost"]
+                )
+                >> select([
+                    "kernel",
+                    "benchmark",
+                    "params",
+                    "ruleset",
+                    "greedy",
+                    "cycles"
+                ])
+            )
+            res.append(df)
+            print(json.dumps(config, indent=2))
+            # print(df[df["kernel"] == "compgen"]["cycles"])
+
+    df = (pd.concat(res)
+          >> sort_values(by=["benchmark", "params"], key=cmp_params)
+          >> reset_index(drop=True, names=["index"])
+          >> display()
+          >> to_csv(
+              Path("figs") / "data" / "est_cycles.csv",
+              index=False
+          ))
+
+
 def compile_times():
     res = []
     for config_path in Path("completed").glob("**/config.json"):
@@ -67,25 +139,27 @@ def compile_times():
         if "metadata" not in config:
             continue
 
-        if all([
+        if all(
+            [
                 "Mar20" in config["date"],
                 "expanding" in config["metadata"]["rules.json"],
-        ]):
+            ]
+        ):
             memory = pd.read_csv(
-                exp_path / "memory.csv",
-                header=None,
-                names=["timestamp", "ram_used"]
+                exp_path / "memory.csv", header=None, names=["timestamp", "ram_used"]
             )
-            df = memory.agg(['min', 'max'])
-            max_ts = df.loc[['max']]['timestamp'].values[0]
-            min_ts = df.loc[['min']]['timestamp'].values[0]
+            df = memory.agg(["min", "max"])
+            max_ts = df.loc[["max"]]["timestamp"].values[0]
+            min_ts = df.loc[["min"]]["timestamp"].values[0]
             killed = "killed" in list(memory["ram_used"].values)
-            res.append([
-                config["name"],
-                "pruning" if config["metadata"]["alt_cost"] else "stock",
-                max_ts - min_ts,
-                killed
-            ])
+            res.append(
+                [
+                    config["name"],
+                    "pruning" if config["metadata"]["alt_cost"] else "stock",
+                    max_ts - min_ts,
+                    killed,
+                ]
+            )
 
     out = Path("figs") / "data" / "compile_times.csv"
     df = pd.DataFrame(res, columns=["benchmark", "type", "runtime", "killed"])
@@ -103,17 +177,13 @@ def scheduler():
         if "metadata" not in config:
             continue
 
-        if all([
-                "Mar23" in config["date"]
-        ]):
+        if all(["Mar23" in config["date"]]):
             memory = pd.read_csv(
-                exp_path / "memory.csv",
-                header=None,
-                names=["timestamp", "ram_used"]
+                exp_path / "memory.csv", header=None, names=["timestamp", "ram_used"]
             )
-            df = memory.agg(['min', 'max'])
-            max_ts = df.loc[['max']]['timestamp'].values[0]
-            min_ts = df.loc[['min']]['timestamp'].values[0]
+            df = memory.agg(["min", "max"])
+            max_ts = df.loc[["max"]]["timestamp"].values[0]
+            min_ts = df.loc[["min"]]["timestamp"].values[0]
             res.append([config["name"], max_ts - min_ts])
 
     # write data to csv
@@ -128,17 +198,16 @@ def play():
         exp_path = Path(config_path.parents[0])
         config = json.load(config_path.open("r"))
 
-        if all([
-                "Mar20" in config["date"]
-        ]):
+        if all(["Mar20" in config["date"]]):
             print(exp_path)
 
 
 def main():
     # exp_iter("2d-conv_3x3_3x3")
+    compile_est_cycles()
     # compile_times()
     # scheduler()
-    play()
+    # play()
 
 
 # if __name__ == "__main__":
