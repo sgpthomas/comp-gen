32b1989a3e8462ae50a2032efcf94b2fc244ff0f
Sammy Thomas <sgpthomas@gmail.com>
2023-08-10 12:54:53 -0500

    update aec build scripts

--- aec/fedora-build-image.sh
+++ aec/fedora-build-image.sh
@@ -6,8 +6,17 @@ contmnt=$(buildah mount $cont)
 
 dnf install --installroot $contmnt \
     --releasever 38 \
-    coreutils bash curl git make cmake gcc "gcc-c++" clang-devel \
+    coreutils bash git make cmake gcc "gcc-c++" clang-devel z3 font-config-devel \
     --setopt install_weak_deps=false -y
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/install-rustup.sh
+CARGO_HOME="$contmnt/root/.cargo" RUSTUP_HOME="$contmnt/root/.rustup" /tmp/install-rustup.sh -y --no-modify-path
+
+# install racket
+wget https://download.racket-lang.org/installers/8.9/racket-8.9-x86_64-linux-cs.sh -O /tmp/install-racket.sh
+sh /tmp/install-racket.sh --in-place --dest "$contmnt/root/racket"
+rm /tmp/install-racket.sh
 
 buildah unmount $cont
 
